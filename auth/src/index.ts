661b32dfd7b0b87b91386a90a48734764fad83d0
KavinduDan <dananjayakavinduedu@gmail.com>
2022-01-05 13:28:32 +0530

    configured ingress for ticketpick.com local

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import axios from "axios";
+import { json } from "body-parser";
+import cors from "cors";
 
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
+app.use(json());
+app.use(cors());
+
+app.get("/api/users/currentuser", (req, res) => {
+  // res.json({ "directrory working": "gg" });
+  res.send("working");
+});
 
 app.listen(3000, () => {
   console.log("auth listening on port 3000!");
