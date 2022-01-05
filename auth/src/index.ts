4f3d7526a1c8b20df77cf62eb878edcd28c656bc
KavinduDan <dananjayakavinduedu@gmail.com>
2022-01-05 12:11:33 +0530

    auth services base created

---
+++ auth/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("auth listening on port 3000!");
+});
